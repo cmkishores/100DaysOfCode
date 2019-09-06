7b274962aecd0497a6f0838326acbf60fa0795e2
cmkishores <cmkishores@gmail.com>
2019-09-06 16:13:31 +0530

    Created a simple web page using django

--- HTML/django/pages/views.py
+++ HTML/django/pages/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+def homepageview(request):
+	return HttpResponse("Welcome to my first webpage ")
 # Create your views here.
